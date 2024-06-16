b177ba21e7a3c33c687ed5852d9a2bfc70be8c59
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-06-16 12:43:52 -0500

    Add: Head line, title and call to action button in hero component

--- src/modules/home/features/hero.js
+++ src/modules/home/features/hero.js
@@ -1,9 +1,10 @@
+import { Button } from "@nextui-org/react";
 import Image from "next/image";
 
 const Hero = () => {
   return (
     <div className="bg-[#f7f5ff] h-[95vh]">
-      <div className="w-full h-full flex justify-center items-center relative overflow-hidden">
+      <div className="w-full h-full flex justify-center items-center relative overflow-hidden px-5">
         <Image
           src={"https://media.beehiiv.com/cdn-cgi/image/fit=scale-down,onerror=redirect,format=auto,width=1920,quality=75/www/homepage/MobileHero.png"}
           alt=""
@@ -12,9 +13,16 @@ const Hero = () => {
           className="w-[80%] object-cover spin-slow"
         />
         <div className="absolute">
-          <h1 className="font-clashDisplay uppercase font-bold text-cyber-ink text-[2.75rem] md:text-[7xl] lg:text-[4rem] xl:text-[5.75rem] max-w-4xl mx-auto text-center z-10">
-            the newsletter platform built for <span className="font-style">GROW</span>
-          </h1>
+          <h2 className="font-clashDisplay uppercase font-bold text-cyber-ink text-[2.5rem] md:text-[7xl] lg:text-[4rem] xl:text-[5.5rem] max-w-4xl mx-auto text-center z-10 mb-5">
+            Affordable Newsletter Designed to Fuel <span className="font-style">Growth</span>
+          </h2>
+          <h3 className="text-3xl text-center mb-5">Built by people who want a better Newsletter alternative</h3>
+          <div className="text-center mb-5">
+            <Button color="primary" className="text-xl py-7 px-8">
+              Get Started
+            </Button>
+          </div>
+          <h4 className="text-center text-lg">start a 30day free trial</h4>
         </div>
       </div>
     </div>
