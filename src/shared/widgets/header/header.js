434a0311c0e85eaf9382ccc8e8e472f60abb9b86
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-06-16 12:45:02 -0500

    Add: Toolbar component to Header

--- src/shared/widgets/header/header.js
+++ src/shared/widgets/header/header.js
@@ -1,10 +1,11 @@
 import Logo from "./logo";
 import Link from "next/link";
 import NavItems from "./nav-items";
+import Toolbar from "./toolbar";
 
 const Header = () => {
   return (
-    <header className="w-full sticky top-0 left-0 z-50 border-b border-b-gray-700 px-10 flex items-center justify-between h-20 bg-white text-black">
+    <header className="w-full sticky top-0 left-0 z-50 shadow shadow-gray-300 px-10 flex items-center justify-between h-20 bg-white text-black">
       <div>
         <Link href={'/'}>
           <Logo />
@@ -12,6 +13,9 @@ const Header = () => {
       </div>
       <div>
         <NavItems />
+      </div>
+      <div className="flex items-center gap-3">
+        <Toolbar />
       </div>
     </header>
   )
